cc211f54c7096f89b58d3a4ff88471a983bbfa2d
sonalkawale3 <sonalkawale3@gmail.com>
2026-07-31 15:32:23 +0530

    first code

---
+++ env/dev/main.tf
@@ -0,0 +1,5 @@
+resource "azurerm_resource_group" "rgs" {
+    name= "rg1"
+    location ="centralindia"
+  
+}
---
+++ env/dev/provider.tf
@@ -0,0 +1,19 @@
+terraform{
+    required_providers {
+      azurerm={
+        source = "hashicorp/azurerm"
+        version = "4.1.0"
+      }
+    }
+backend "azurerm" {
+    storage_account_name = "pipelinekeliye"
+    resource_group_name = "for-backend"
+    container_name = "pipestorage"
+    key = "dev12.state"  
+}
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "05dbb74f-6152-4a1d-a1fd-c49be5c3fd99"
+}
