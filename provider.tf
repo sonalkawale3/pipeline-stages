7a5fc21eeef9860b5d7fef8bed9579498816204e
sonalkawale3 <sonalkawale3@gmail.com>
2026-08-01 14:02:00 +0530

    backend added

--- provider.tf
+++ provider.tf
@@ -6,6 +6,12 @@ terraform{
       }
     }
 
+backend "azurerm"{
+  storage_account_name = "pipelinekeliye"
+  resource_group_name ="for-backend"
+  container_name ="pipestorage"
+  key ="dev12.tfstate"
+}
 }
 
 provider "azurerm" {
